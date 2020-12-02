51719ecbdae7d5b5544bb1a89a9b0a5dd8b5ab95
Alexandre ZITOUNI TINLOT <58300805+AlexandreZT@users.noreply.github.com>
2020-12-02 16:17:10 +0100

    updates astrub complet

--- README.md
+++ README.md
@@ -4,17 +4,17 @@ Quelques scripts pour automatiser des tâches sur le jeu dofus.
 
 ## dobot_miner.py (facilement extensible à d'autres mines)
 
-Script qui permet de miner le Fer de la zone "Mine Hable"
+Script qui permet de miner le Fer de la zone d'Astrub
 1) Vous devez être à l'entrer de "Mine Hable"
 2) Il suffit de renseigner le niveau de votre métier et le nombre d'aller-retour à effectuer
 3) Directement retourner sur la fenêtre dofus, le script va se lancer.
 
-D'autres mines seront peut-être développé.
+D'autres mines pourront seront peut-être développé.
 
 module : pyautogui
 
 ## dobot_marchand.py (en cours)
 
-Script qui permet de se mettre en mode marchand dès que le script détecte qu'une personne s'est retiré de son mode marchand
+Script qui permet de se mettre en mode marchand dès quele script détecte qu'une personne s'est retiré de son mode marchand
 
 module : pyautogui, figlet
--- dobot_miner.py
+++ dobot_miner.py
@@ -77,7 +77,7 @@ class mine(object):
     def decision(self, pos, empty_color):
         pyautogui.click(pos[0], pos[1])
         if empty_color != mine.can_recolt():
-            time.sleep(uniform(8, 9))
+            time.sleep(uniform(9, 10)) # temps pour déplacement et recolte
             loop=False
     
     def make_decision(self):
@@ -88,10 +88,11 @@ class mine(object):
         time.sleep(2)
         x, y = pyautogui.position()
         time.sleep(1)
-        while True:
+        loop = True
+        while loop:
             try:
                 print((x, y),", ", pyautogui.pixel(x, y))
-                exit(0)
+                loop =  False
             except:
                 pass
 
@@ -109,12 +110,10 @@ class astrub_souterrains_profonds(mine):
             
         ]
 
-class astrub_mine_hable(mine):
+class astrub(mine):
     def __init__(self):
         self.mine_position = [10, -19]
         self.path = [
-            [475, 435], # monter
-            [482, 303], # descendre
             [1340, 370], # entrer dans la mine
             [1470, 250], # nouvelle salle
             [1617, 389], # salle à droite (todo 2 cuivre)
@@ -125,10 +124,53 @@ class astrub_mine_hable(mine):
             [597, 945], # je redescent
             [365, 851], # retour première salle
             [300, 715], # sortir de la mine
-            [265, 635], # voir à gauche
-            [1763, 540], # revenir à droite
+            [1457, 949], # en bas
+            [220, 158], # gauche
+            [1288, 37], # monter
+            [254, 304], # gauche
+            [1511, 955], # bas
+            [1511, 955], # bas
+            [1648, 422], # droite (recolte) 
+            [205, 494], # gauche    
+            [939, 950], # bas
+            [1658, 482], # droite
+            [1646, 531], # droite (recolte)
+            [412, 956], # bas 10 -15
+            [148, 467], # gauche
+            [148, 467], # gauche    
+            [148, 467], # gauche
+            [148, 467], # gauche
+            [148, 467], # gauche
+            [148, 467], # gauche
+            [148, 467], # gauche
+            [148, 467], # gauche
+            [148, 467], # gauche
+            [1567, 36], # haut
+            [821, 39], # haut
+            [1152, 567], # entrer mine
+            [1604, 295], # salle droite
+            [1518, 689], # salle droite
+            [380, 293], # retour gauche
+            [336, 844], # retour gauche
+            [261, 979], # sortie mine
+            [666, 37], # haut
+            [940, 43], # haut
+            [1025, 41], # haut
+            [979, 38], # haut
+            [1697, 338], # droite (1,-21)
+            [1697, 338], # droite 2,-21)
+            [1697, 600], # droite 3,-21)
+            [1697, 600], # droite 4,-21)
+            [1697, 600], # droite 5,-21)
+            [1697, 600], # droite 6,-21)    
+            [1697, 600], # droite 7,-21)
+            [1728, 247], # droite (mine) 8,-21)
+            [694, 949], # bas
+            [795, 945], # bas
+            [1568, 257], # droite (mine)
+            [482, 303], # descendre           
         ]
-        self.availableOre = ["Cuivre", "Fer"] # etc. Y'en a d'autres
+        self.availableOre = ["Kobalte", "Bronze", "Cuivre", "Fer"] # Y'en a d'autres mais le reste faut être abonné
     
     def getAvailableOre(self):
         return self.availableOre
@@ -139,11 +181,36 @@ class astrub_mine_hable(mine):
     
     def miner(self):
         for room in self.path: # pour chaque pièce
-            pyautogui.click(room) # changement de pièce
-            time.sleep(uniform(7, 8)) # temps pour changer de pièce
+            self.mouvement(room) # changement de pièce
+            # mine.make_decision()
             for Ore in mine.getOreList(metier_level): # pour toutes les ressouces récoltables (par ordre de niveau)
                 if Ore in self.availableOre: # les récolter par priorité
-                    astrub_mine_hable.take_resources(room, Ore) # clique pour récolter
+                    astrub.take_resources(room, Ore) # clique pour récolter
+
+    def mouvement(self, room):
+        pyautogui.click(room) # clique pour bouger
+        # if room == [1340, 370]:
+        #     wait = True
+        #     while wait:
+        #         try:
+        #             if pyautogui.pixel(room[0], room[1]) == (153, 133, 16):
+        #                 print("nice")
+        #                 wait = False
+        #             else:
+        #                 time.sleep(0.25)
+        #         except:
+        #             print("no painno gain")
+        # else:
+        time.sleep(uniform(8, 9)) # temps pour changer de pièce
+
+    def check_pods(self):
+        pass
+
+    def go_bank(self):
+        pass
+
+    def back_mine_hable(self):
+        pass
 
     def take_resources(self, room, Ore):
         """
@@ -151,13 +218,13 @@ class astrub_mine_hable(mine):
         sinon next
         """
         if Ore == "Fer":
-            astrub_mine_hable.take_fer(room)
+            astrub.take_fer(room)
         
         elif Ore == "Cuivre":
-            astrub_mine_hable.take_cuivre(room)
+            astrub.take_cuivre(room)
 
         elif Ore == "Bronze":
-            astrub_mine_hable.take_bronze(room)
+            astrub.take_bronze(room)
 
     def take_fer(self, room):
         """
@@ -166,7 +233,7 @@ class astrub_mine_hable(mine):
         Sinon (autre pixel) alors je peux next ou attendre qu'il se déplace # quelque chose bloque l'accès (attention combat ou joueur)
         S'il y a plusieurs room, c'est pcq parfois il revient sur ses pas durant sa tournée
         """
-        if room == [475, 435]:
+        if room == [475, 435] or room == [1568, 257]:
             mine.decision((721, 93), (187, 182, 137))
             mine.decision((752, 130), (163, 158, 118))
 
@@ -179,27 +246,57 @@ class astrub_mine_hable(mine):
         elif room == [1470, 250] or room == [597, 945] or room == [335, 805]:
             mine.decision((694, 634), (80, 68, 41))
 
-        elif room == [1617, 389]: # salle à droite (todo 2 cuivre)
+        elif room == [1617, 389]:
             pass
-        elif room == [1043, 168]: # je monte
+        elif room == [1043, 168]:
             mine.decision((775, 750), (84, 69, 32))
             mine.decision((1239, 579), (35, 28, 13))
         elif room == [300, 715]:
             pass
-        elif room == [265, 635]:
+        elif room == [1288, 37] or room == [795, 945]:
             mine.decision((1228, 526), (110, 97, 55))
             mine.decision((1175, 560), (96, 87, 55))
             mine.decision((538, 395),(115, 113, 80))
             mine.decision((1060, 229), (86, 77, 49))
-            pyautogui.click(1106, 207) # end
         elif room == [1763, 540]:
             pass
+        elif room == [1457, 949] or room == [795, 945]:
+            mine.decision((829, 143), (131, 127, 22))
+        elif room == [1648, 422]:
+            mine.decision((424, 328), (144, 142, 104))
+            mine.decision((616, 267), (204, 198, 144))
+            mine.decision((665, 294), (122, 120, 88))
+            mine.decision((714, 309), (182, 179, 126))
+        elif room == [1646, 531]:
+            mine.decision((1296, 385), (113, 107, 86))
+            mine.decision((1546, 453), (199, 190, 140))
+            mine.decision((616, 444), (185, 182, 133))
+            mine.decision((947, 347), (200, 192, 140))
+        elif room == [1152, 567] or room == [336, 844]:
+            mine.decision((392, 618), (64, 55, 27))
+            mine.decision((637, 508), (33, 28, 15))
+            mine.decision((683, 461), (39, 33, 18))
+            mine.decision((1021, 303), (23, 20, 10))
+            mine.decision((1306, 443) , (70, 57, 1))
+        elif room == [1604, 295] or room == [380, 293]:
+            mine.decision((506, 424), (95, 77, 37))
+            mine.decision((646, 335) ,  (43, 35, 19))
+            mine.decision((937, 195), (13, 10, 5))
+            mine.decision((1270, 159) ,  (15, 18, 6))
+            mine.decision((1421, 311), (25, 27, 4))
+            mine.decision((1091, 524), (67, 56, 0))
+        elif room == [1518, 689]:
+            mine.decision((1009, 183), (57, 43, 11))
+            mine.decision((674, 502), (12, 9, 2))
 
     def take_cuivre(self, room):
         print("tu ne peux pas miner le cuivre")
     
     def take_bronze(self, room):
         print("tu ne peux pas miner le bronze")
+
+    def take_kobalte(self, room):
+        print("tu ne peux pas miner le kobalte")
 
 class combat(object):
     """
@@ -226,7 +323,8 @@ if __name__ == "__main__":
     developing = False
     if developing == True:
         mine = mine()
-        mine.make_decision()
+        while developing:
+            mine.make_decision()
 
     metier_level = eval(input("Donne-moi le niveau de ton métier : "))
     aller_retour = eval(input("Donne-moi le nombre d'aller-retour à effecter : "))
@@ -235,10 +333,11 @@ if __name__ == "__main__":
     time.sleep(uniform(4, 6))
 
     mine = mine()
-    astrub_mine_hable = astrub_mine_hable()
+    astrub = astrub()
 
     récolter = True
     
     while récolter and aller_retour > 0:
-        astrub_mine_hable.miner()
+        astrub.miner()
         aller_retour-=1
+        print(aller_retour, "aller-retour restant")
